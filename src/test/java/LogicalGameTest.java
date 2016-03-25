16cec9872f3d877f246bd81eac9496c967ed5d73
Kristalev Danil <kristalev2@gmail.com>
2016-03-25 12:43:54 +0600

    TDD Добавлен новый тест на передвижение ячейки, но он не работаетю.

--- src/test/java/LogicalGameTest.java
+++ src/test/java/LogicalGameTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  */
 public class LogicalGameTest {
     @Test
-    public void testMoving(){
+    public void testMoving1(){
         int[][] pole =
                 {{1,1,1,1},    //пустая ячейка по координатам (2,2)
                 {1,1,1,1},
@@ -16,5 +16,18 @@ public class LogicalGameTest {
         Game game = new Game(pole);
         game.move(2,3); //двигаем единичку, она должна переехать на пустую ячейку.
         Assert.assertEquals("Не правильно поехало!",game.getCell(2,2),1);
+    }
+
+    //еще один тест на передвижения. Теперь все ячейки разные
+    @Test
+    public void testMoving2(){
+        int[][] pole =
+                        {{1,2,3,4},    //пустая ячейка по координатам (2,2)
+                        {5,6,7,8},
+                        {9,10,0,11},
+                        {12,13,14,15}};
+        Game game = new Game(pole);
+        game.move(2,3); //двигаем 11, она должна переехать на пустую ячейку.
+        Assert.assertEquals("Не правильно поехало!",game.getCell(2,2),11);
     }
 }
